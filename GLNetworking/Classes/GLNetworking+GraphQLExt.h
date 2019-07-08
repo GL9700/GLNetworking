fb22d5182b480ff0b17fddf462bced2cd427ab50
liguoliang <guoliang@51jianjiao.com>
2019-07-08 19:12:09 +0800

    fix: methodName for head

--- GLNetworking/Classes/GLNetworking+GraphQLExt.h
+++ GLNetworking/Classes/GLNetworking+GraphQLExt.h
@@ -13,10 +13,10 @@ NS_ASSUME_NONNULL_BEGIN
 @interface GLNetworking (GraphQLExt)
 
 /** GraphQL Query 方法  content-Type:Application/Json */
-- (NSDictionary *)gQueryStringWithMethod:(NSString *)method params:(NSDictionary<NSString *, NSObject *> *)params returns:(NSArray<NSString *> *)returns;
++ (NSDictionary *)gQueryStringWithMethod:(NSString *)method params:(NSDictionary<NSString *, NSObject *> *)params returns:(NSArray<NSString *> *)returns;
 
 /** GraphQL Mutation 方法  content-Type:Application/Json */
-- (NSDictionary *)gMutationStringWithMethod:(NSString *)method variables:(NSDictionary *)variables returns:(NSArray<NSString *> *)returns;
++ (NSDictionary *)gMutationStringWithMethod:(NSString *)method variables:(NSDictionary *)variables returns:(NSArray<NSString *> *)returns;
 
 @end
 
